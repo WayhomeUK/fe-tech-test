a06b3b503d1b32456f966161f7a4856cfb3ade9d
jo1807 <49209412+jo1807@users.noreply.github.com>
2021-07-21 11:09:22 +0100

    add typescript

---
+++ src/locations/locations.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+import { locationsList } from "./data/location-data";
+
+function Locations() {
+  return (
+    <Wrapper>
+      <Header>Locations</Header>
+      {/* add your locations here */}
+    </Wrapper>
+  );
+}
+
+export default Locations;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Header = styled.h1`
+  align-self: center;
+`;
