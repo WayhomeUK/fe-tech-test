bcd280392737f60b61a170bc9d481677485b6f78
jo1807 <49209412+jo1807@users.noreply.github.com>
2021-07-22 13:49:56 +0100

    add data

---
+++ src/data/location-data.js
@@ -0,0 +1,506 @@
+export const locationsList = [
+  {
+    name: "Dunstable",
+    county: "Bedfordshire",
+  },
+  {
+    name: "Leighton Buzzard",
+    county: "Bedfordshire",
+  },
+  {
+    name: "Ascot",
+    county: "Berkshire",
+  },
+  {
+    name: "Bracknell",
+    county: "Berkshire",
+  },
+  {
+    name: "Maidenhead",
+    county: "Berkshire",
+  },
+  {
+    name: "Reading",
+    county: "Berkshire",
+  },
+
+  {
+    name: "Windsor",
+    county: "Berkshire",
+  },
+  {
+    name: "Wokingham",
+    county: "Berkshire",
+  },
+  {
+    name: "Avonmouth",
+    county: "Bristol",
+  },
+  {
+    name: "Bedminster",
+    county: "Bristol",
+  },
+  {
+    name: "Bishopsworth",
+    county: "Bristol",
+  },
+  {
+    name: "Whitchurch",
+    county: "Bristol",
+  },
+  {
+    name: "Filton",
+    county: "Bristol",
+  },
+  {
+    name: "Milton Keynes",
+    county: "Buckinghamshire",
+  },
+  {
+    name: "Aylesbury",
+    county: "Buckinghamshire",
+  },
+  {
+    name: "Bletchley",
+    county: "Buckinghamshire",
+  },
+  {
+    name: "Newport Pagnell",
+    county: "Buckinghamshire",
+  },
+  {
+    name: "St Neots",
+    county: "Cambridgeshire",
+  },
+  {
+    name: "St Ives",
+    county: "Cambridgeshire",
+  },
+  {
+    name: "Huntingdon",
+    county: "Cambridgeshire",
+  },
+  {
+    name: "Macclesfield",
+    county: "Cheshire",
+  },
+  {
+    name: "Bollington",
+    county: "Cheshire",
+  },
+  {
+    name: "Knutsford",
+    county: "Cheshire",
+  },
+  {
+    name: "Exeter",
+    county: "Devon",
+  },
+  {
+    name: "Exmouth",
+    county: "Devon",
+  },
+  {
+    name: "Bournemouth",
+    county: "Dorset",
+  },
+  {
+    name: "Poole",
+    county: "Dorset",
+  },
+  {
+    name: "Brighton",
+    county: "East Sussex",
+  },
+  {
+    name: "Portslade",
+    county: "East Sussex",
+  },
+  {
+    name: "Basildon",
+    county: "Essex",
+  },
+  {
+    name: "Loughton",
+    county: "Essex",
+  },
+
+  {
+    name: "Waltham Abbey",
+    county: "Essex",
+  },
+  {
+    name: "Brentwood",
+    county: "Essex",
+  },
+  {
+    name: "Buckhurst Hill",
+    county: "Essex",
+  },
+  {
+    name: "Canvey Island",
+    county: "Essex",
+  },
+  {
+    name: "Chelmsford",
+    county: "Essex",
+  },
+  {
+    name: "Chigwell",
+    county: "Essex",
+  },
+  {
+    name: "Colchester",
+    county: "Essex",
+  },
+  {
+    name: "Rayleigh",
+    county: "Essex",
+  },
+  {
+    name: "Rochford",
+    county: "Essex",
+  },
+  {
+    name: "Shoeburyness",
+    county: "Essex",
+  },
+  {
+    name: "South Benfleet",
+    county: "Essex",
+  },
+  {
+    name: "Southend-on-Sea",
+    county: "Essex",
+  },
+  {
+    name: "Stanford-le-Hope",
+    county: "Essex",
+  },
+  {
+    name: "Wickford",
+    county: "Essex",
+  },
+  {
+    name: "Gloucester",
+    county: "Gloucestershire",
+  },
+  {
+    name: "Yate",
+    county: "Gloucestershire",
+  },
+  {
+    name: "Bromley",
+    county: "Greater London",
+  },
+  {
+    name: "Carshalton",
+    county: "Greater London",
+  },
+  {
+    name: "Dagenham",
+    county: "Greater London",
+  },
+  {
+    name: "Selsdon",
+    county: "Greater London",
+  },
+  {
+    name: "Belmont",
+    county: "Greater London",
+  },
+  {
+    name: "North Cheam",
+    county: "Greater London",
+  },
+  {
+    name: "South Norwood",
+    county: "Greater London",
+  },
+  {
+    name: "Mitcham",
+    county: "Greater London",
+  },
+  {
+    name: "Purley",
+    county: "Greater London",
+  },
+  {
+    name: "Romford",
+    county: "Greater London",
+  },
+  {
+    name: "Sutton",
+    county: "Greater London",
+  },
+  {
+    name: "Morden",
+    county: "Greater London",
+  },
+  {
+    name: "Sidcup",
+    county: "Greater London",
+  },
+  {
+    name: "Orpington",
+    county: "Greater London",
+  },
+  {
+    name: "Caterham",
+    county: "Greater London",
+  },
+  {
+    name: "Upminster",
+    county: "Greater London",
+  },
+  {
+    name: "Uxbridge",
+    county: "Greater London",
+  },
+  {
+    name: "Warlingham",
+    county: "Greater London",
+  },
+  {
+    name: "Stretford",
+    county: "Greater Manchester",
+  },
+  {
+    name: "Sale",
+    county: "Greater Manchester",
+  },
+  {
+    name: "Stockport",
+    county: "Greater Manchester",
+  },
+  {
+    name: "Altrincham",
+    county: "Greater Manchester",
+  },
+  {
+    name: "Wilmslow",
+    county: "Greater Manchester",
+  },
+  {
+    name: "Aldershot",
+    county: "Hampshire",
+  },
+  {
+    name: "Blackwater",
+    county: "Hampshire",
+  },
+  {
+    name: "Fleet",
+    county: "Hampshire",
+  },
+  {
+    name: "Tadley",
+    county: "Hampshire",
+  },
+  {
+    name: "Farnborough",
+    county: "Hampshire",
+  },
+  {
+    name: "Petersfield",
+    county: "Hampshire",
+  },
+  {
+    name: "Alton",
+    county: "Hampshire",
+  },
+  {
+    name: "Basingstoke",
+    county: "Hampshire",
+  },
+  {
+    name: "Totton",
+    county: "Hampshire",
+  },
+  {
+    name: "Eastleigh",
+    county: "Hampshire",
+  },
+  {
+    name: "Waterlooville",
+    county: "Hampshire",
+  },
+  {
+    name: "Winchester",
+    county: "Hampshire",
+  },
+  {
+    name: "Southampton",
+    county: "Hampshire",
+  },
+  {
+    name: "Chandlers Ford",
+    county: "Hampshire",
+  },
+  {
+    name: "Yateley",
+    county: "Hampshire",
+  },
+  {
+    name: "Berkhamsted",
+    county: "Hertfordshire",
+  },
+  {
+    name: "Broxbourne",
+    county: "Hertfordshire",
+  },
+  {
+    name: "Hemel Hempstead",
+    county: "Hertfordshire",
+  },
+  {
+    name: "Hoddesdon",
+    county: "Hertfordshire",
+  },
+  {
+    name: "Waltham Cross",
+    county: "Hertfordshire",
+  },
+
+  {
+    name: "Hertford",
+    county: "Hertfordshire",
+  },
+  {
+    name: "Ware",
+    county: "Hertfordshire",
+  },
+  {
+    name: "Ashford",
+    county: "Kent",
+  },
+  {
+    name: "Faversham",
+    county: "Kent",
+  },
+  {
+    name: "Sittingbourne",
+    county: "Kent",
+  },
+  {
+    name: "West Malling",
+    county: "Kent",
+  },
+  {
+    name: "Canterbury",
+    county: "Kent",
+  },
+  {
+    name: "Chatham",
+    county: "Kent",
+  },
+  {
+    name: "Gillingham",
+    county: "Kent",
+  },
+  {
+    name: "Herne Bay",
+    county: "Kent",
+  },
+  {
+    name: "Maidstone",
+    county: "Kent",
+  },
+  {
+    name: "Rainham",
+    county: "Kent",
+  },
+  {
+    name: "Rochester",
+    county: "Kent",
+  },
+  {
+    name: "Strood",
+    county: "Kent",
+  },
+
+  {
+    name: "Whitstable",
+    county: "Kent",
+  },
+  {
+    name: "Abingdon-on-Thames",
+    county: "Oxfordshire",
+  },
+  {
+    name: "Banbury",
+    county: "Oxfordshire",
+  },
+  {
+    name: "Bicester",
+    county: "Oxfordshire",
+  },
+  {
+    name: "Carterton",
+    county: "Oxfordshire",
+  },
+  {
+    name: "Kidlington",
+    county: "Oxfordshire",
+  },
+  {
+    name: "Oxford",
+    county: "Oxfordshire",
+  },
+  {
+    name: "Woking",
+    county: "Surrey",
+  },
+  {
+    name: "Addlestone",
+    county: "Surrey",
+  },
+  {
+    name: "Chertsey",
+    county: "Surrey",
+  },
+  {
+    name: "Camberley",
+    county: "Surrey",
+  },
+  {
+    name: "Farnham",
+    county: "Surrey",
+  },
+  {
+    name: "West Byfleet",
+    county: "Surrey",
+  },
+  {
+    name: "Royal Leamington Spa",
+    county: "Warwickshire",
+  },
+  {
+    name: "Shoreham-by-Sea",
+    county: "West Sussex",
+  },
+  {
+    name: "Southwick",
+    county: "West Sussex",
+  },
+  {
+    name: "Burn Bridge",
+    county: "Yorkshire",
+  },
+  {
+    name: "Garforth",
+    county: "Yorkshire",
+  },
+  {
+    name: "Harrogate",
+    county: "Yorkshire",
+  },
+  {
+    name: "Leeds",
+    county: "Yorkshire",
+  },
+  {
+    name: "Yeadon",
+    county: "Yorkshire",
+  },
+];
